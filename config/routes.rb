29de6f8d3f3bfb1ca185522ab8717ced7ac1d04c
Vinícius Crepschi <vibracre@gmail.com>
2019-07-02 19:01:28 -0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :users
+  resources :products
   resources :sessions, only: [:new, :create, :destroy]
 
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
-  
+  get 'empresta', to: 'products#index', as: 'empresta'
+
 end

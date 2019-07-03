4348ecf9e77572132817a17104bc386531135e4a
lucasToniello <lucasgranja@hotmail.com>
2019-07-03 13:31:27 +0000

    Tela de listar produtos feita

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,6 +28,7 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
+        puts(@product)
         format.html { redirect_to @product, notice: 'Product was successfully created.' }
         format.json { render :show, status: :created, location: @product }
       else

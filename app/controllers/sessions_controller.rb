203074a9948c2395e6d0eec09539398e8f05ba38
Vinícius Crepschi <vibracre@gmail.com>
2019-07-02 23:00:39 -0300

    session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, notice: "Logado!"
+      redirect_to root_url
     else
       flash.now[:alert] = "Email ou senha invalidos!"
       render "new"
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, notice: "Deslogado!"
+    redirect_to root_url
   end
   
 end

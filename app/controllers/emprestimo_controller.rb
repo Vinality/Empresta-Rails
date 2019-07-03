d8d5ae4b32e9bc148ba6342d5a2e895842598f26
lucasToniello <lucasgranja@hotmail.com>
2019-07-03 15:56:29 +0000

    Model de empréstimo modificado, ainda não funciona

--- app/controllers/emprestimo_controller.rb
+++ app/controllers/emprestimo_controller.rb
@@ -1,16 +1,36 @@
 class EmprestimoController < ApplicationController
+    before_action :set_product, :set_user
 
     def create
-    @user, @product = Emprestimo.new(user_params, product_params)
+      @user, @product = Emprestimo.new(user_params, product_params)
 
-    respond_to do |format|
-      if @product.save
-        format.html { redirect_to @product, notice: 'Emprestimo was successfully created.' }
-        format.json { render :show, status: :created, location: @product }
-      else
-        format.html { render :new }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
+      respond_to do |format|
+        if @product.save
+          format.html { redirect_to @product, notice: 'Emprestimo was successfully created.' }
+          format.json { render :show, status: :created, location: @product }
+        else
+          format.html { render :new }
+          format.json { render json: @product.errors, status: :unprocessable_entity }
+        end
       end
     end
-  end
+
+    private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    def product_params
+      params.require(:product).permit(:name, :url, :description)
+    end
+
 end
--- app/models/emprestimo.rb
+++ app/models/emprestimo.rb
@@ -1,4 +1,4 @@
-class Lend < ApplicationRecord
+class Emprestimo < ApplicationRecord
   has_one :user, :class_name => "user"
   has_one :product, :class_name => "product"
 end
